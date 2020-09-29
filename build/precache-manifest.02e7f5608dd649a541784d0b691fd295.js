d8c696661d21d436833c14c2cefb11629aa1091a
mhabib4 <mostafa.habib@optum.com>
2020-09-28 21:17:13 -0400

    refactor code

---
+++ build/precache-manifest.02e7f5608dd649a541784d0b691fd295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd71da4d7603c647c6ece03129f233e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c3f13c06c6998f05b25",
+    "url": "/static/css/main.ff87a77a.chunk.css"
+  },
+  {
+    "revision": "2cc03c440bf286072e10",
+    "url": "/static/js/2.c068cca6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c068cca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3f13c06c6998f05b25",
+    "url": "/static/js/main.0a3d4386.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
